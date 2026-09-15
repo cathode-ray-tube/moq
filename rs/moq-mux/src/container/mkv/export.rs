e46d03556b2a1abea743c425889e4b7c51b45869
cathode-ray-tube <cathode-ray222@proton.me>
2026-09-16 00:19:51 +0100

    Fix typo in FrameDecrypter import

--- rs/moq-mux/src/container/mkv/export.rs
+++ rs/moq-mux/src/container/mkv/export.rs
@@ -13,7 +13,7 @@ use crate::catalog::Stream;
 use crate::container::ExportSource;
 use crate::container::Frame;
 use crate::container::mkv::Error;
-use crate::container::FrameDecrytper;
+use crate::container::FrameDecrypter;
 
 /// Matroska TimestampScale: 1 ms (in nanoseconds).
 const TIMESTAMP_SCALE_NS: u64 = 1_000_000;
