73db966becf009dc047d7d10a8be1e97531b5c5f
cathode-ray-tube <cathode-ray222@proton.me>
2026-09-16 00:34:43 +0100

    Update poll_next to use decrypter_factory

--- rs/moq-mux/src/codec/h264/export.rs
+++ rs/moq-mux/src/codec/h264/export.rs
@@ -83,7 +83,9 @@ impl<S: Stream> Export<S> {
 	pub fn poll_next(&mut self, waiter: &kio::Waiter) -> Poll<crate::Result<Option<Bytes>>> {
 		while let Some(catalog) = self.catalog.as_mut() {
 			match catalog.poll_next(waiter)? {
-				Poll::Ready(Some(snapshot)) => self.update_catalog(&snapshot.media())?,
+				Poll::Ready(Some(snapshot)) => {
+    self.update_catalog(&snapshot.media(), &decrypter_factory)?
+}
 				Poll::Ready(None) => {
 					self.catalog = None;
 					break;
